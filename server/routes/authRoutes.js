c0c01962c8eed8393f9cbe0960d244a332dfd407
Rohit Sahu <sahu86744@gmail.com>
2024-07-29 02:51:45 +0530

    updated UploadUserInfo

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, profile } = require('../controllers/authController');
+const { register, login, profile, UploadUserInfo } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
